b12c6bb0ddb2bf7f3854a1023cfe37c80fa6a1c9
JudeHyde <judeasikogu@gmail.com>
2022-09-08 12:27:58 -0700

    File sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: prints the size of various types on the computer it is
  * compiled and run on
- * 
+ *
  * Return: Always 0 (Success)
  */
 
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	
+
 	return (0);
 }
