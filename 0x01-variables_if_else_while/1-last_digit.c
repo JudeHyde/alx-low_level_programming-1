5ec1663e4716e8818d5dfe55803ddb0a11e39cac
JudeHyde <judeasikogu@gmail.com>
2022-09-09 14:08:28 -0700

    Check positive or negative for a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - determine if number is positive or negative
  *
- * Description: program checks if last digit of a number is greater or less than five
+ * Description: program checks if last digit is greater or less than five
  *
  * Return: Always 0 (Success)
  */
@@ -13,15 +13,15 @@
 int main(void)
 {
 	int n;
-	
+	dig = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, dig);
 	else if (n % 10 < 6)
-	       	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dig);
 	else if (n % 10 == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, dig);
 	return (0);
 }
