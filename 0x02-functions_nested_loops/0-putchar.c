b292f7c34ae67895b9fa92ca50a7963dba7a59b8
JudeHyde <judeasikogu@gmail.com>
2022-09-13 05:20:19 -0700

    Prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Programe to print putchar
+ *
+ * Description: Programme to print putchar
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	{
+		printf("_putchar");
+	}
+
+	return (0);
+}
