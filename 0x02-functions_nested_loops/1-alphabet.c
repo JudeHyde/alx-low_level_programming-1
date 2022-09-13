4a32b8f164734f71aeb63775f575c1cb75ffdc3f
JudeHyde <judeasikogu@gmail.com>
2022-09-13 16:19:22 -0700

    Print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@
  * Return: Always 0 (Success)
  */
 
-	void print_alphabet(void)
+	int print_alphabet(void)
 {
 	int i = 97;
 
@@ -18,7 +18,7 @@
 	{
 		putchar(i);
 		i++;
-	} return (void);
+	} return (0);
 
 }
 
