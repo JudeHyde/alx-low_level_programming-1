d0d74f3e55b8cae8b1ac2243cbb5b9ca253c2d31
JudeHyde <judeasikogu@gmail.com>
2022-09-14 14:46:57 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - This returns absolute
+ * @n: Operand for checking
+ *
+ * Description: Programm to return absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int digit = n % 10;
+
+	return (digit);
+}
+
