98bffa319aa947fdee3dc22dea39466636b4146b
JudeHyde <judeasikogu@gmail.com>
2022-09-14 06:20:29 -0700

    Finds lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _islower - This checks for case
+ * @c: Operand for checking
+ *
+ * Description: Programm to chack alphabets casing
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int _islower(int c);
+
+{
+	if  (islower(c) == 0;)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
