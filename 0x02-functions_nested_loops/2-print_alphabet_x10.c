4b6005750fe0230aa2ec576ca40b7361e4c8c1c1
JudeHyde <judeasikogu@gmail.com>
2022-09-14 04:45:13 -0700

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_alphabet_x10 - This prints the alhpabets
+ *
+ * Description: Programm to print alphabets 10 times using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+
+	void print_alphabet_x10(void)
+{
+	int rollover = 0;
+
+	while (rollover < 10)
+	{
+		int i = 97;
+		
+		while (i < 123)
+			{
+			putchar(i);
+			i++;
+			}
+		putchar(10);
+		rollover++;
+	} 
+}
+
