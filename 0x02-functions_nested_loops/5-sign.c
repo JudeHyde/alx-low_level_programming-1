812ff1d05a753f777edd3f347559879165be0c89
JudeHyde <judeasikogu@gmail.com>
2022-09-14 11:00:02 -0700

    Checks sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_sign - This checks for sign
+ * @n: Operand for checking
+ *
+ * Description: Programm to check for sign of integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar(45);
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	}
+}
+
