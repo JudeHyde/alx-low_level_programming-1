695c4c179bd6009f495c409618bc0f3d2ea8101d
JudeHyde <judeasikogu@gmail.com>
2022-09-14 17:48:24 -0700

    Prints any number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints numbers under 98
+ *
+ * @n1: integer 1
+ *
+ * Description: Prints all numbers under 98
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		printf(%d, n);
+		n++;
+	} 
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+		printf(%d, n);
+		n--;
+		}
+	}
+	printf("98\n")
+}
