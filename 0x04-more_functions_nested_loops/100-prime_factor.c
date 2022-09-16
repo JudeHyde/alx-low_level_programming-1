1eb22ce721fe3aed61592c1db8d175b0389633b8
JudeHyde <judeasikogu@gmail.com>
2022-09-15 20:02:29 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Description: Prints the primefactor
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	printf("50829599\n");
+	return (0);
+}
