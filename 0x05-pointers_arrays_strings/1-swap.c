7b6a400c13efaa32f91b091bb3ad990f1d786643
JudeHyde <judeasikogu@gmail.com>
2022-09-19 07:45:22 -0700

    Swap two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,9 +8,9 @@
  * Return: void
  */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-	int  firstswap = *a;
+	int firstswap = *a;
 	int secondswap = *b;
 	*a = secondswap;
 	*b = firstswap;
